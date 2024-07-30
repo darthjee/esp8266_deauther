bdfadd6a9e119f2046495a6d20b53bd386122c3e
darthjee <darthjee@gmail.com>
2024-07-30 19:50:01 -0300

    Rubocop fix

--- utils/font_helper/spec/support/factories/font_binaries.rb
+++ utils/font_helper/spec/support/factories/font_binaries.rb
@@ -15,14 +15,14 @@ FactoryBot.define do
       height { 28 }
       first_character { 48 }
       characters_count { 10 }
-      
+
       character_bytes { (height / 8.0).ceil }
       character_width { width }
 
       characters_table do
         characters_count.times.map do |index|
           start_position  = index * character_bytes
-          [start_position / 256, start_position  % 256, character_bytes, character_width ]
+          [start_position / 256, start_position % 256, character_bytes, character_width]
         end
       end
 
