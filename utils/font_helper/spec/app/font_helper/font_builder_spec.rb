bd4dcc9baef80f46eb37972e3f1bea1fcfcf3a21
darthjee <darthjee@gmail.com>
2024-07-30 18:48:38 -0300

    Add factory

--- utils/font_helper/spec/app/font_helper/font_builder_spec.rb
+++ utils/font_helper/spec/app/font_helper/font_builder_spec.rb
@@ -5,33 +5,32 @@ require 'spec_helper'
 describe FontHelper::FontBuilder do
   let(:width) { 24 }
   let(:height) { 32 }
-  let(:first_char) { 48 }
-  let(:char_size) { Random.rand(2..10) }
-  let(:char_width) { Random.rand(10..30) }
-  let(:characters) do
-    char_count.times.map do |index|
-      start_position  = index * char_size
-      [start_position / 256, start_position  % 256, char_size, char_width ]
-    end
+  let(:first_character) { 48 }
+  let(:character_bytes) { Random.rand(2..10) }
+  let(:character_width) { Random.rand(10..30) }
+
+  let(:font_binary) do
+    build(
+      :font_binary,
+      width:, height:, first_character:, characters_count:,
+      character_bytes:, character_width:
+    )
   end
-  let(:characters_binary) do
-    (char_count * char_size).times.map { 100 }
-  end
-  let(:binaries) { characters + characters_binary }
+
   let(:expected_font) do
     FontHelper::Font.new(width:, height:)
   end
 
   context 'when no char data is given' do
-    let(:char_count) { 0 }
+    let(:characters_count) { 0 }
 
     it do
-      expect(described_class.build(width, height, first_char, char_count, *binaries))
+      expect(described_class.build(*font_binary))
         .to be_a(FontHelper::Font)
     end
 
     it "Returns Font with expected attributes" do
-      expect(described_class.build(width, height, first_char, char_count, *binaries))
+      expect(described_class.build(*font_binary))
         .to eq(expected_font)
     end
   end
