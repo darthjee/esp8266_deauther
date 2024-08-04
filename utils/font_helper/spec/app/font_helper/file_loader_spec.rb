9bc1699ccb4b21df005d2ba839920c28b4b8a01e
darthjee <darthjee@gmail.com>
2024-08-04 11:46:44 -0300

    Rubocop fix

--- utils/font_helper/spec/app/font_helper/file_loader_spec.rb
+++ utils/font_helper/spec/app/font_helper/file_loader_spec.rb
@@ -5,21 +5,21 @@ require 'spec_helper'
 describe FontHelper::FileLoader do
   subject(:loader) { described_class }
 
-  let(:path) { "spec/support/fixtures/font.txt" }
+  let(:path) { 'spec/support/fixtures/font.txt' }
   let(:expected_data) do
     [
-      24,28,32,4,
-      255,255,0,7,
-      0,0,1,7,
-      0,1,2,7,
-      0,3,1,7,
+      24, 28, 32, 4,
+      255, 255, 0, 7,
+      0, 0, 1, 7,
+      0, 1, 2, 7,
+      0, 3, 1, 7,
       170,
       221, 218,
       202
     ]
   end
 
-  describe ".read" do
+  describe '.read' do
     it do
       expect(loader.read(path))
         .to eq(expected_data)
--- utils/font_helper/spec/app/font_helper/font_loader_spec.rb
+++ utils/font_helper/spec/app/font_helper/font_loader_spec.rb
@@ -5,7 +5,7 @@ require 'spec_helper'
 describe FontHelper::FontLoader do
   subject(:loader) { described_class }
 
-  let(:path) { "spec/support/fixtures/font.txt" }
+  let(:path) { 'spec/support/fixtures/font.txt' }
   let(:expected_font) do
     FontHelper::Font.new(width: 24, height: 28, characters: expected_characters)
   end
@@ -14,11 +14,11 @@ describe FontHelper::FontLoader do
       FontHelper::Character.new(code: 32, width: 7, binary: nil),
       FontHelper::Character.new(code: 33, width: 7, binary: [170]),
       FontHelper::Character.new(code: 34, width: 7, binary: [221, 218]),
-      FontHelper::Character.new(code: 35, width: 7i, binary: [202]),
+      FontHelper::Character.new(code: 35, width: 7i, binary: [202])
     ]
   end
 
-  describe ".load" do
+  describe '.load' do
     it do
       expect(loader.load(path))
         .to eq(expected_font)
