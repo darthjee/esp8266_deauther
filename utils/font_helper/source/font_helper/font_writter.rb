1fbb25d79704476dfc51400355e12fa9c064162d
darthjee <darthjee@gmail.com>
2024-08-04 17:17:22 -0300

    Add labels

--- utils/font_helper/source/font_helper/font_writter.rb
+++ utils/font_helper/source/font_helper/font_writter.rb
@@ -27,8 +27,7 @@ class FontHelper
     def write
       ATTRIBUTES.each { |attr| write_attr(attr) }
 
-      file.write("\n  // Jump Table:\n")
-      file.write("  // start point (1), start point (2), data size, block width\n")
+      write_jump_table
       write_characters
 
       file.close
@@ -36,7 +35,14 @@ class FontHelper
 
     private
 
+    def write_jump_table
+      file.write("\n  // Jump Table:\n")
+      file.write("  // start point (1), start point (2), data size, block width\n")
+    end
+
     def write_characters
+      file.write("\n  // Font Data:\n")
+      
       characters.keys.sort.each do |code|
         character = font.character(code)
         CharacterBinaryWritter.write(character, file)
