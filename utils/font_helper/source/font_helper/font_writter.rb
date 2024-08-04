541799e07da9dd4d428a70a271bda5c53f2747eb
darthjee <darthjee@gmail.com>
2024-08-04 16:17:48 -0300

    Writte attribute

--- utils/font_helper/source/font_helper/font_writter.rb
+++ utils/font_helper/source/font_helper/font_writter.rb
@@ -16,11 +16,17 @@ class FontHelper
     end
 
     def write
-      file.write("#{width.to_s(16)} // Width: #{width}\n")
+      write_attr(:width)
       file.close
     end
 
     private
+
+    def write_attr(attribute)
+      value = public_send(attribute)
+      hex = value.to_s(16)
+      file.write("  0x#{hex}, // Width: #{value}\n")
+    end
 
     def file
       @file ||= File.open(path, "w")
