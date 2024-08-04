9daa6f0c2d15fdc2bc1de7cef54fbc435b1b6fc4
darthjee <darthjee@gmail.com>
2024-08-04 18:24:46 -0300

    Add missing spec

--- utils/font_helper/spec/app/font_helper/character_spec.rb
+++ utils/font_helper/spec/app/font_helper/character_spec.rb
@@ -3,14 +3,34 @@
 require 'spec_helper'
 
 describe FontHelper::Character do
-  subject(:character) { build(:character, code:) }
+  subject(:character) { build(:character, code:, binary:) }
+
+  let(:binary) { nil }
+  let(:code)   { 48 }
 
   describe '#character' do
     context 'when code is a valid character' do
-      let(:code) { 48 }
-
       it 'returns the character from code' do
         expect(character.character).to eq('0')
+      end
+    end
+  end
+
+  describe '#binary' do
+    context 'when it has been initialized' do
+      let(:binary) { [32, 25] }
+
+      it 'returns the binary' do
+        expect(character.binary)
+          .to eq(binary)
+      end
+    end
+
+    context 'when it has not been initialized' do
+      let(:binary) { nil }
+
+      it 'returns an empty array' do
+        expect(character.binary).to eq([])
       end
     end
   end
