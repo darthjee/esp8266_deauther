8c704705ddf14160ddf4ecfe8750ba14757fe8ca
darthjee <darthjee@gmail.com>
2024-08-14 16:56:52 -0300

    Refactor code

--- utils/font_helper/source/font_helper/binary_converter.rb
+++ utils/font_helper/source/font_helper/binary_converter.rb
@@ -3,16 +3,10 @@
 class FontHelper
   module BinaryConverter
     def self.to_bits(byte)
-        [
-          byte & 1,
-          (byte & 2) / 2,
-          (byte & 4) / 4,
-          (byte & 8) / 8,
-          (byte & 16) /16,
-          (byte & 32) / 32,
-          (byte & 64) / 64,
-          (byte & 128) / 128,
-        ]
+      8.times.map do |exp|
+        power = 2 ** exp
+        (byte & power) / power
+      end
     end
   end
 end
