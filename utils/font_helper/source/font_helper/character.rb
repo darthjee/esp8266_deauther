e818d97b241bad06192cdd4335cea317853e5d1f
darthjee <darthjee@gmail.com>
2024-08-14 15:51:42 -0300

    Add bitmap

--- utils/font_helper/source/font_helper/character.rb
+++ utils/font_helper/source/font_helper/character.rb
@@ -23,5 +23,24 @@ class FontHelper
     def binary
       @binary ||= []
     end
+
+    def bitmap
+      @bitmap ||= generate_bitmap
+    end
+
+    def generate_bitmap
+      binary.map do |byte|
+        [
+          byte & 1,
+          (byte & 2) / 2,
+          (byte & 4) / 4,
+          (byte & 8) / 8,
+          (byte & 16) /16,
+          (byte & 32) / 32,
+          (byte & 64) / 64,
+          (byte & 128) / 128,
+        ]
+      end
+    end
   end
 end
--- utils/font_helper/spec/app/font_helper/character_spec.rb
+++ utils/font_helper/spec/app/font_helper/character_spec.rb
@@ -34,4 +34,22 @@ describe FontHelper::Character do
       end
     end
   end
+
+  describe 'bitmap' do
+    context 'when binary describes a single column' do
+      let(:binary) { [255] }
+
+      it 'returns the remapping into a bitmap' do
+        expect(character.bitmap).to eq([[1,1,1,1,1,1,1,1]])
+      end
+    end
+
+    context 'when binary is not all 1' do
+      let(:binary) { [170] }
+
+      it 'returns the remapping into a bitmap' do
+        expect(character.bitmap).to eq([[0,1,0,1,0,1,0,1]])
+      end
+    end
+  end
 end
