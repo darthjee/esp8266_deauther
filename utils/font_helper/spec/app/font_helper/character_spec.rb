b69987f1642670412150d99aebe206a06c8555ff
darthjee <darthjee@gmail.com>
2024-08-14 21:05:05 -0300

    Add byte_height

--- utils/font_helper/spec/app/font_helper/character_spec.rb
+++ utils/font_helper/spec/app/font_helper/character_spec.rb
@@ -3,10 +3,11 @@
 require 'spec_helper'
 
 describe FontHelper::Character do
-  subject(:character) { build(:character, code:, binary:) }
+  subject(:character) { build(:character, code:, binary:, height:) }
 
   let(:binary) { nil }
   let(:code)   { 48 }
+  let(:height) { 28 }
 
   describe '#character' do
     context 'when code is a valid character' do
@@ -31,6 +32,40 @@ describe FontHelper::Character do
 
       it 'returns an empty array' do
         expect(character.binary).to eq(FontHelper::BitMap.new)
+      end
+    end
+  end
+
+  describe '#byte_height' do
+    context "when height is 8 or less" do
+      let(:height) { Random.rand(1..8) }
+
+      it do
+        expect(character.byte_height).to eq(1)
+      end
+    end
+
+    context "when height is ibetween 9 and 16" do
+      let(:height) { Random.rand(9..16) }
+
+      it do
+        expect(character.byte_height).to eq(2)
+      end
+    end
+
+    context "when height is ibetween 17 and 24" do
+      let(:height) { Random.rand(17..24) }
+
+      it do
+        expect(character.byte_height).to eq(3)
+      end
+    end
+
+    context "when height is ibetween 25 and 32" do
+      let(:height) { Random.rand(25..32) }
+
+      it do
+        expect(character.byte_height).to eq(4)
       end
     end
   end
