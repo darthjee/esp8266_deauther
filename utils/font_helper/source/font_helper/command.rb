c8195b541488b8b636867b78221fb08d60b07334
darthjee <darthjee@gmail.com>
2024-08-18 12:15:21 -0300

    Handle keys

--- utils/font_helper/source/font_helper/command.rb
+++ utils/font_helper/source/font_helper/command.rb
@@ -11,9 +11,29 @@ class FontHelper
 
     delegate :font, :font=, to: :script
 
-    def self.for(command:, script:, arguments:)
-      klass = const_get(command)
-      klass.new(script, *arguments)
+    class << self
+      def for(command:, script:, arguments:)
+        klass = const_get(command)
+
+        args = arguments_list(arguments)
+        opts = options(arguments).symbolize_keys
+
+        klass.new(script, *args, **opts)
+      end
+
+      def arguments_list(arguments)
+        return [] if arguments.is_a?(Hash)
+
+        arguments.reject { |arg| arg.is_a?(Hash) }
+      end
+
+      def options(arguments)
+        return arguments if arguments.is_a?(Hash)
+
+        arguments.select { |arg| arg.is_a?(Hash) }.map.with_object({}) do |opt, hash|
+          hash.merge!(opt)
+        end
+      end
     end
 
     attr_reader :arguments, :script
--- utils/font_helper/spec/app/font_helper/script_spec.rb
+++ utils/font_helper/spec/app/font_helper/script_spec.rb
@@ -37,10 +37,23 @@ describe FontHelper::Script do
       end
     end
 
-    context 'when scripts loads and edits a font' do
+    context 'when scripts loads and deletes characters' do
       let(:path) { 'spec/support/fixtures/script_removing_characters.yml' }
       let(:expected_result_file) do
         'spec/support/fixtures/font_lacking_characters.txt'
+      end
+
+      it 'loads all commands and write the font' do
+        script.run
+
+        expect(File.read(output_path)).to eq(expected_content)
+      end
+    end
+
+    context 'when scripts loads and crops characters' do
+      let(:path) { 'spec/support/fixtures/script_croping.yml' }
+      let(:expected_result_file) do
+        'spec/support/fixtures/font_croped.txt'
       end
 
       it 'loads all commands and write the font' do
