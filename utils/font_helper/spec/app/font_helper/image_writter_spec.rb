e29a631f3beee8a8715f7c8d5cfb3f3121e23207
darthjee <darthjee@gmail.com>
2024-08-19 17:04:19 -0300

    Tet file writting

--- utils/font_helper/spec/app/font_helper/image_writter_spec.rb
+++ utils/font_helper/spec/app/font_helper/image_writter_spec.rb
@@ -3,13 +3,15 @@
 require 'spec_helper'
 
 describe FontHelper::ImageWritter do
-  let(:font)      { FontHelper::FontLoader.load(font_path) }
-  let(:font_path) { 'spec/support/fixtures/font_simplified.txt' }
-  let(:code)      { 48 }
-  let(:output)    { "spec/support/fixtures/#{code}.pbm" }
-  let(:character) { font.character(code) }
+  let(:font)        { FontHelper::FontLoader.load(font_path) }
+  let(:font_path)   { 'spec/support/fixtures/font_simplified.txt' }
+  let(:code)        { (48..58).to_a.sample }
+  let(:output)      { "/tmp/#{code}_#{SecureRandom.hex(16)}.pbm"}
+  let(:character)   { font.character(code) }
+  let(:sample_path) { "spec/support/fixtures/#{code}.pbm" }
+  let(:sample)      { File.read(sample_path) }
 
-  before do
+  after do
     File.delete(output) if File.exist?(output)
   end
 
@@ -17,5 +19,11 @@ describe FontHelper::ImageWritter do
     expect { described_class.write(character, output) }
       .to change { File.exist?(output) }
       .from(false).to(true)
+  end
+
+  it 'creates the file with the correct content' do
+    described_class.write(character, output)
+
+    expect(File.read(output)).to eq(sample)
   end
 end
