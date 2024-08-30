b5a9ba7377dda878dd57050d0bf7dce10852e0e3
darthjee <darthjee@gmail.com>
2024-08-30 12:22:07 -0300

    Use setter

--- esp8266_deauther/CLI.cpp
+++ esp8266_deauther/CLI.cpp
@@ -1245,10 +1245,10 @@ void CLI::runCommand(String input) {
     // ===== SCREEN ===== //
     // screen mode <menu/packetmonitor/buttontest/loading>
     else if (eqlsCMD(0, CLI_SCREEN) && eqlsCMD(1, CLI_MODE)) {
-        if (eqlsCMD(2, CLI_MODE_BUTTONTEST)) displayUI.mode = DISPLAY_MODE::BUTTON_TEST;
-        else if (eqlsCMD(2, CLI_MODE_PACKETMONITOR)) displayUI.mode = DISPLAY_MODE::PACKETMONITOR;
-        else if (eqlsCMD(2, CLI_MODE_LOADINGSCREEN)) displayUI.mode = DISPLAY_MODE::LOADSCAN;
-        else if (eqlsCMD(2, CLI_MODE_MENU)) displayUI.mode = DISPLAY_MODE::MENU;
+        if (eqlsCMD(2, CLI_MODE_BUTTONTEST)) displayUI.setMode(DISPLAY_MODE::BUTTON_TEST);
+        else if (eqlsCMD(2, CLI_MODE_PACKETMONITOR)) displayUI.setMode(DISPLAY_MODE::PACKETMONITOR);
+        else if (eqlsCMD(2, CLI_MODE_LOADINGSCREEN)) displayUI.setMode(DISPLAY_MODE::LOADSCAN);
+        else if (eqlsCMD(2, CLI_MODE_MENU)) displayUI.setMode(DISPLAY_MODE::MENU);
         else parameterError(list->get(2));
         prntln(CLI_CHANGED_SCREEN);
     }
